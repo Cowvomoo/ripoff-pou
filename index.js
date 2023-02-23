b0c82c596c9ada8f52a497c81fd14423b2c41507
Cowvomoo <40768010+Cowvomoo@users.noreply.github.com>
2023-02-23 14:37:44 -0500

    1 file modified

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const readline = require('readline');
 const Gilbert = require('./gilbertschema.js');
 
 const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+
 mongoose
   .connect(
     'mongodb+srv://spektree1:BeginningMeepCityDos2@cluster0.tpyi0wq.mongodb.net/Gilbert?retryWrites=true&w=majority'
@@ -20,20 +23,29 @@ const rl = readline.createInterface({
 let gilb;
 const login = (name) => {
   return new Promise((resolve, reject) => {
-    gilb = Gilbert.find((x) => x === name);
+    //
+    Gilbert.find({ name: name }).then((res) => (gilb = res));
     console.log(gilb);
-    if (gilb === []) {
-      reject('xd');
-    } else {
+    //
+    if (gilb) {
+      console.log('gilb resolved');
       resolve('xd');
+    } else {
+      gilb = 'test';
+      console.log('no gilb');
+      reject('xd');
     }
+    //
   });
 };
 
 rl.question('Please Enter Your Name to Log In \n', (res) => {
   let tempName = res;
-  login(res).then(
-    console.log(`You have been logged in as ${gilb.name}`).catch((x) => {
+  login(res)
+    .then(() => {
+      console.log(`You have been logged in as ${gilb.name}`);
+    })
+    .catch(() => {
       console.log(`User Not Found`);
       rl.question(
         'Would You Like To Create a New Gilbert?\n (Please Respond Y or N) \n',
@@ -41,12 +53,12 @@ rl.question('Please Enter Your Name to Log In \n', (res) => {
           if (res === 'Y') {
             gilb = new Gilbert();
             gilb.name = tempName;
+            gilb.save();
           } else {
             console.log('oh... ok! restart program if you change your mind \n');
           }
         }
       );
-    })
-  );
+    });
 });
 //
